f384615a1987dd1a9aa8312e3c72c42e76ab8454
cjrzh <cjrzh@163.com>
2016-07-11 09:19:34 +0800

    解决登陆注册等页面中文显示问题

--- b2c/settings.py
+++ b2c/settings.py
@@ -106,9 +106,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'zh-cn'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai' 
 
 USE_I18N = True
 
